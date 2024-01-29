c54a13686938afd984315dba3091fe38ee055813
Shalom91 <shalom.mothapo@gmail.com>
2024-01-29 08:28:42 +0200

    inintial commit

---
+++ src/main/java/com/enviro/assessment/grad001/shalommothapo/Enviro365investmentsApplication.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.shalommothapo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Enviro365investmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Enviro365investmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enviro/assessment/grad001/shalommothapo/Enviro365investmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.assessment.grad001.shalommothapo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Enviro365investmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
