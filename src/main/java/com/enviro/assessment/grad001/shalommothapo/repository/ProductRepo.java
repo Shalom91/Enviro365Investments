aaab41504370e8ab88a77232e006625bfbd4e17f
Shalom Mothapo (SM) <Mothapsm@telkom.co.za>
2024-01-30 13:18:16 +0200

    add custom method

--- src/main/java/com/enviro/assessment/grad001/shalommothapo/repository/ProductRepo.java
+++ src/main/java/com/enviro/assessment/grad001/shalommothapo/repository/ProductRepo.java
@@ -4,6 +4,9 @@ import com.enviro.assessment.grad001.shalommothapo.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepo extends JpaRepository<Product, Long> {
+    List<Product> findByInvestorId(Long investorId);
 }
