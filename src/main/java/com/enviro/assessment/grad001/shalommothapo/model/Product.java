36073229d2fa846d543550df5986eb80a3659589
Shalom Mothapo (SM) <Mothapsm@telkom.co.za>
2024-01-30 13:07:02 +0200

    add Product entity class

---
+++ src/main/java/com/enviro/assessment/grad001/shalommothapo/model/Product.java
@@ -0,0 +1,22 @@
+package com.enviro.assessment.grad001.shalommothapo.model;
+
+import com.enviro.assessment.grad001.shalommothapo.enums.ProductType;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Product {
+    @Id
+    @Column(columnDefinition = "LONG AUTO_INCREMENT")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private ProductType type;
+
+    @ManyToOne
+    private Investor investor;
+}
